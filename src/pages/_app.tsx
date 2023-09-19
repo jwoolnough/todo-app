255b320ad9172af9763dab6ab3c3746757f688c0
Jordan Woolnough <jordan.woolnough@hotmail.co.uk>
2023-09-19 13:58:51 +0100

    Add fonts and base styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,49 @@
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
+import localFont from "next/font/local";
 
 import { api } from "@/utils/api";
 
 import "@/styles/globals.css";
 
-const MyApp: AppType<{ session: Session | null }> = ({
+const jones = localFont({
+  src: [
+    {
+      path: "../styles/fonts/jones-light.woff2",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../styles/fonts/jones-book.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../styles/fonts/jones-medium.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../styles/fonts/jones-bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+  variable: "--font-jones",
+});
+
+const TodoApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <div className={`${jones.variable} font-primary`}>
+        <Component {...pageProps} />
+      </div>
     </SessionProvider>
   );
 };
 
-export default api.withTRPC(MyApp);
+export default api.withTRPC(TodoApp);
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,36 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        slate: {
+          400: "#abb1ca",
+          700: "#31313c",
+          800: "#27272d",
+          900: "#161618",
+          950: "#050505",
+        },
+        green: {
+          "50": "#f3faf4",
+          "100": "#e4f4e7",
+          "200": "#c9e9d1",
+          "300": "#9fd6ae",
+          "400": "#6eba83",
+          "500": "#50ac69",
+          "600": "#38814c",
+          "700": "#2f663f",
+          "800": "#295235",
+          "900": "#23442d",
+          "950": "#0f2416",
+        },
+      },
+      fontFamily: {
+        primary: ["var(--font-jones)", "sans-serif"],
+      },
+      borderColor: {
+        DEFAULT: "#1d1d21",
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
