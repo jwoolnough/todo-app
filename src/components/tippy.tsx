d261c5e0cfccc6068b13ddb98b75f1813ab51205
Jordan Woolnough <jordan@migratehr.com>
2023-09-21 14:38:39 +0100

    Add tippy animations

--- src/components/tippy.tsx
+++ src/components/tippy.tsx
@@ -1,5 +1,6 @@
 import BaseTippy, { type TippyProps } from "@tippyjs/react";
 import { forwardRef, useEffect, useState } from "react";
+import "tippy.js/animations/shift-away.css";
 import "tippy.js/dist/tippy.css";
 
 import { clsxm } from "@/utils/clsxm";
@@ -7,7 +8,7 @@ import { clsxm } from "@/utils/clsxm";
 const Tippy = forwardRef<HTMLElement, TippyProps>(
   ({ className, ...rest }, ref) => {
     // This is horrible and a pain just to get tippy fonts to work - either need to
-    // switch to app router or stop using next/font to load fonts
+    // switch to app router or stop using `next/font` to load fonts
     const [appElement, setAppElement] = useState<HTMLElement | null>(null);
 
     useEffect(() => {
@@ -20,6 +21,7 @@ const Tippy = forwardRef<HTMLElement, TippyProps>(
           "border bg-slate-700 text-xs [&>.tippy-arrow]:text-slate-700",
           className,
         )}
+        animation="shift-away"
         {...(appElement && { appendTo: appElement })}
         {...rest}
         ref={ref}
