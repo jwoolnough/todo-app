0ec2c4b19c815f8d53f0d3a638de6031c00957b8
Jordan Woolnough <jordan.woolnough@hotmail.co.uk>
2024-06-15 13:50:17 +0100

    Fix titles when week spans two months

--- src/features/schedule/header/title.tsx
+++ src/features/schedule/header/title.tsx
@@ -1,16 +1,29 @@
 "use client";
 
-import { format } from "date-fns";
+import { endOfWeek, format } from "date-fns";
+
+import { startOfWeek } from "~/utils";
 
 import { useDateQuery } from "../hooks/use-date-query";
+
+const getWeekTitle = (selectedDate: Date) => {
+  const startOfWeekDate = startOfWeek(selectedDate);
+  const endOfWeekDate = endOfWeek(selectedDate);
+
+  if (startOfWeekDate.getMonth() === endOfWeekDate.getMonth()) {
+    return format(startOfWeekDate, "MMMM y");
+  }
+
+  return `${format(startOfWeekDate, "MMMM")} - ${format(endOfWeekDate, "MMMM y")}`;
+};
 
 const ScheduleTitle = () => {
   const { selectedWeekDate } = useDateQuery();
 
   return (
     <>
-      <h1>{format(selectedWeekDate, "MMMM y")}</h1>
-      <p className="[@media(width<380px)]:hidden">
+      <h1>{getWeekTitle(selectedWeekDate)}</h1>
+      <p className="[@media(width<23.75em)]:hidden">
         Week {format(selectedWeekDate, "w")}
       </p>
     </>
