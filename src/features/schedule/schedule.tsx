f1065a0167dbf3220f1c1a4785119b381c6a3bb0
Jordan Woolnough <jordan.woolnough@hotmail.co.uk>
2024-08-28 10:36:45 +0100

    On mobile, add space below bottom grid so nav doesn't overlap col-end items

--- src/features/schedule/schedule.tsx
+++ src/features/schedule/schedule.tsx
@@ -11,7 +11,7 @@ import { TimeSidebar } from "./time-sidebar";
 
 const Schedule = () => {
   return (
-    <div className="relative grid w-min min-w-full grid-cols-[3rem_repeat(7,minmax(9.375rem,1fr))_1rem] grid-rows-[min-content_repeat(32,2rem)_1rem] gap-[var(--gap)] [--gap:0.3125rem]">
+    <div className="relative grid w-min min-w-full grid-cols-[3rem_repeat(7,minmax(9.375rem,1fr))_1rem] grid-rows-[min-content_repeat(32,2rem)_1rem] gap-[var(--gap)] [--gap:0.3125rem] max-md:pb-14">
       <PastDays />
 
       <DayHeader />
